19dcc5299db02247aa861b7b9541ad12655a3284
Abdulhamid <73017095+Abdulhamid51@users.noreply.github.com>
2026-01-27 07:50:00 +0500

    Update AppConfig for fast_model app
    Renamed CommonModelsConfig to FastModelConfig, set default_auto_field to 'django.db.models.BigAutoField', and added a verbose_name for improved app configuration.

--- fast_model/apps.py
+++ fast_model/apps.py
@@ -1,4 +1,6 @@
 from django.apps import AppConfig
 
-class CommonModelsConfig(AppConfig):
+class FastModelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'fast_model'
+    verbose_name = 'Fast Model Management'
